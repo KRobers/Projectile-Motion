75d0a741cebbfb3bf0b0c7ef43e81ab90f13f2dd
Kaj Robers <kaj.robers@gmail.com>
2021-01-12 21:43:31 +0100

    Integratie Matplotlib // Toevoegingen: Parabool functie met formule / table met x en y coordinaten / forloop voor het maken van de parabool // Fouten: Parabool top komt niet overeen met de maxheight functie

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
+import matplotlib.pyplot as plt
 import math
-speed = int(input("What is the projectile speed? (m/s) > "))
-angle = int(input("What is the angle of the projectile? > "))
+speed = float(input("What is the projectile speed? (m/s) > "))
+angle = float(input("What is the angle of the projectile? > "))
 
 g = 9.81
 
@@ -13,6 +14,34 @@ def timeToHeight(speed, angle, g):
     timeh = (speed * math.sin(angle)) / g
     return timeh
 
+def parabool(speed, angle, g, i):
+    yfor = float(math.tan(angle)) * i - (g / (float(2) * (speed ** float(2) ) * (float(math.cos(angle))))) * (i ** float(2) )
+    return yfor
 
 print("The max height of the projectile will be: " + str(maxHeight(speed, angle, g)) + " meters")
 print("The time to reach the max height will be: " + str(timeToHeight(speed, angle, g)) + " seconds")
+
+# x axis values
+x = [0.0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5]
+y = []
+
+for i in x:
+    yinner = parabool(speed, angle, g, i)
+    y.append(yinner)
+    print(i)
+    print(yinner)
+
+
+# plotting the points
+plt.plot(x, y)
+
+# naming the x axis
+plt.xlabel('x - Tijd')
+# naming the y axis
+plt.ylabel('y - Hoogte')
+
+# giving a title to my graph
+plt.title('Projectile Motion')
+
+# function to show the plot
+plt.show()
